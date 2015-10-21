e5047e88ce8d3e641a70676b18e0a4f8a6a40bd2
Niru4 <nxj115@gmail.com>
2015-10-21 10:42:31 -0500

    publishing deck

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+library(devtools)
+library(slidify)
+setwd("/Users/MALI/Documents/Coursera/courses/09_DevelopingDataProducts/project/assigment/")
+author("data_products_assignment")
+slidify("index.Rmd")
+browseURL("index.html")
+publish(user="Niru4", repo= "Data-Products")
